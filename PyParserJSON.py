807132b52e4df709171a0e6035648384357a2129
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2017-03-31 09:57:09 +0900

    use OpenWeatherMapAPI
    use openweather api instead of openAPI

--- PyParserJSON.py
+++ PyParserJSON.py
@@ -1,32 +1,14 @@
-#OpenAPI
-'''
-                                                                                                                                                                                                        import xml.etree.ElementTree as ET
-import urllib.request
-from xml.etree.ElementTree import parse
+#-*-coding:utf-8
 
-def subper(sub) :
-    url = "http://data.jeju.go.kr/rest/JejuLdapsDataService/getForecastPointDataXY"
 
-    tree = ET.ElementTree(file=urllib.request.urlopen(url))
-    root = tree.getroot()
-
-    return root
-
-print(subper("nx"))
-'''
-import json
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
-url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib'
-service_key = 'NgWWeOgssOHILkuc0IgP9n93E9fBCOAftdEqhYEibSDKxGH0oIff49VSTkYiNh1hFaEpQbt4%2BvRhSAW3mANIKw%3D%3D'
-Now_date = 20170329
-Now_time = '0600'
-nx = 60
-ny = 157
+url = 'http://api.openweathermap.org/data/2.5/forecast'
+service_key = 'de82d6d4bc81413bce54f58ec194befb'
+cityid = '1835847'
 
 
-queryParams = '?' + urlencode({quote_plus('ServiceKey') : service_key},{quote_plus('base_date') : Now_date, quote_plus('base_time') : Now_time, quote_plus('NX') : nx, quote_plus('NY') : ny,quote_plus('numOfRows') : 10,quote_plus('pageNO') : 1,quote_plus('_type') : 'json'})
-
+queryParams = '?' + urlencode({quote_plus('id') : cityid}) + '&' + urlencode({quote_plus('APPID') : service_key})
 request = Request(url + queryParams)
 request.get_method = lambda: 'GET'
 response_body = urlopen(request).read()
