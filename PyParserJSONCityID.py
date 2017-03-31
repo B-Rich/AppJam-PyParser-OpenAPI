78a93cccc1e3462789e8a11d1ebd0ed8bc39e89e
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2017-03-31 14:41:49 +0900

    Parser Use City ID
    City ID Search instead X Y

---
+++ PyParserJSONCityID.py
@@ -0,0 +1,16 @@
+#-*-coding:utf-8
+
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus
+url = 'http://api.openweathermap.org/data/2.5/weather'
+service_key = 'de82d6d4bc81413bce54f58ec194befb'
+cityid = '1835847' #seoul
+lat = 37.56826
+lon = 126.977829
+
+queryParams = '?' + urlencode({quote_plus('id') : cityid}) + '&' + urlencode({quote_plus('APPID') : service_key})
+request = Request(url + queryParams)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+
+print(response_body)
