c44f14c76e0caeef0f16e7e70055c31a98bbd8d0
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2017-04-01 09:56:58 +0900

    Complete Parse Module
    Run excute(True, <YOUR API KEY>)

--- OpenWeatherPaserModule.py
+++ OpenWeatherPaserModule.py
@@ -6,7 +6,7 @@ import json
 import time
 import ast
 
-def APIParse(service_key, cityid, doc):
+def APIParse(service_key, cityid, doc): #요청을 보내고 결과를 받는 함수
     if doc == True:
         print('''APIParse(KEY, ID, DOC-TRUE-FALSE, RUN-TRUE-FALSE)''')
     url = 'http://api.openweathermap.org/data/2.5/weather'
@@ -33,16 +33,34 @@ def APIParse(service_key, cityid, doc):
 
     return weather, avertemp,temp, humidity, pressure, time.time()
 
-def excute(run, key):
+def excute(run, key, city):#파서 함수를 실행
+    find = True
+    count = 0
 
     with open('city.list.kr.json') as citylistjson:
+        #with문은 도시 목록을 불러옴
         citylist = json.load(citylistjson)
         citylist = ast.literal_eval(str(citylist))
-        print(citylist)
+
+        while find == True:
+            #입력받은 도시의 코드 찾기
+            tmp = citylist[count]
+            if tmp["name"] == city:
+                print(tmp.get("_id"))
+                find = False
+                #찾으면 반복문 종료
+            count += 1
 
     while run == True:
-        data = APIParse(key, '1835847', True)
+        data = APIParse(key, '1835847', False)
         print(data)
         time.sleep(1800)
+        #30분에 한번씩 요청을 보낸다.
 
-excute(True, "e439f48431e739fcfd6c3127c1d0d582")
+#예시 요청
+excute(True, "e439f48431e739fcfd6c3127c1d0d582", True)
+
+'''
+excute함수에서 APIParse를 호풀해서 사용한다
+ 이그제큐트만 호출해서 사용하세요.
+'''
